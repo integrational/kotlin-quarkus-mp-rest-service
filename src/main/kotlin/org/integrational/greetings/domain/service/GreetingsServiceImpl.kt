1ba2c6949c4d47da778fd6e4eb6e89a16e920802
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-04 19:52:13 +0200

    using constructor injection

--- src/main/kotlin/org/integrational/greetings/domain/service/GreetingsServiceImpl.kt
+++ src/main/kotlin/org/integrational/greetings/domain/service/GreetingsServiceImpl.kt
@@ -6,7 +6,7 @@ import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
 @ApplicationScoped
-class GreetingsServiceImpl(@Inject private val repo: GreetingsRepo) : GreetingsService {
+class GreetingsServiceImpl @Inject constructor(private val repo: GreetingsRepo) : GreetingsService {
     override fun add(name: String) = repo.createOrUpdate(Greeting(null, name, "Hello $name"))
     override fun get(name: String) = repo.findByName(name)
     override fun getAll() = repo.findAll()

3ebaddc4b942e9e8b50565eb7616076cbf70c718
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-05 09:43:37 +0200

    polishing

--- src/main/kotlin/org/integrational/greetings/domain/service/GreetingsServiceImpl.kt
+++ src/main/kotlin/org/integrational/greetings/domain/service/GreetingsServiceImpl.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 
 @ApplicationScoped
 class GreetingsServiceImpl @Inject constructor(private val repo: GreetingsRepo) : GreetingsService {
-    override fun add(name: String) = repo.createOrUpdate(Greeting(null, name, "Hello $name"))
+    override fun add(name: String) = repo.createOrUpdate(Greeting(null, name, "Hello $name!"))
     override fun get(name: String) = repo.findByName(name)
     override fun getAll() = repo.findAll()
 }

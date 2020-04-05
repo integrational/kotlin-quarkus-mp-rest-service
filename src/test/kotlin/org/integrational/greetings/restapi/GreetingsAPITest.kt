4fdf520f2fa0c214605826835015394e4dfb5af9
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-05 14:44:57 +0200

    further improved REST API integration tests

--- src/test/kotlin/org/integrational/greetings/restapi/GreetingsAPITest.kt
+++ src/test/kotlin/org/integrational/greetings/restapi/GreetingsAPITest.kt
@@ -12,6 +12,9 @@ import java.util.*
 class GreetingsAPITest {
 
     private fun getThen(path: String, status: Int = 200) = given().`when`().get(path).then().statusCode(status)
+    private fun postThen(name: String, status: Int = 201) = given().`when`()
+        .contentType(JSON).body("""{"name":"$name"}""").post("/greetings")
+        .then().statusCode(status)
 
     @Test
     fun openAPIDefPublished() = getThen("/openapi").body(containsString("openapi"))
@@ -33,11 +36,26 @@ class GreetingsAPITest {
         val name = UUID.randomUUID().toString()
         val path = "/greetings/$name"
         getThen(path, 204)
-        given().`when`()
-            .contentType(JSON).body("""{"name":"$name"}""").post("/greetings")
-            .then()
-            .statusCode(201)
-            .header("Location", containsString(path))
+        postThen(name).header("Location", containsString(path))
         getThen(path).body(notNullValue())
+    }
+
+    @Test
+    fun testAddDuplicateAlsoCaseInsensitive() {
+        val name1 = UUID.randomUUID().toString().toUpperCase()
+        val name2 = name1.toLowerCase()
+        val path1 = "/greetings/$name1"
+        val path2 = "/greetings/$name2"
+        getThen(path1, 204)
+        getThen(path2, 204)
+        postThen(name1).header("Location", containsString(path1))
+        getThen(path1).body(notNullValue())
+        getThen(path2).body(notNullValue())
+        postThen(name1, 409)
+        getThen(path1).body(notNullValue())
+        getThen(path2).body(notNullValue())
+        postThen(name2, 409)
+        getThen(path1).body(notNullValue())
+        getThen(path2).body(notNullValue())
     }
 }

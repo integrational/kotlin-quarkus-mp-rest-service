b09d172056bacdd5ea09512e4f0144c10e7d0426
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-05 11:21:15 +0200

    correct implementation and OpenAPI definition of POST to add Greeting

--- src/main/kotlin/org/integrational/greetings/restapi/GreetingsAPI.kt
+++ src/main/kotlin/org/integrational/greetings/restapi/GreetingsAPI.kt
@@ -2,17 +2,25 @@ package org.integrational.greetings.restapi
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
 import org.eclipse.microprofile.openapi.annotations.Operation
+import org.eclipse.microprofile.openapi.annotations.headers.Header
 import org.eclipse.microprofile.openapi.annotations.info.Contact
 import org.eclipse.microprofile.openapi.annotations.info.Info
+import org.eclipse.microprofile.openapi.annotations.media.Content
 import org.eclipse.microprofile.openapi.annotations.media.Schema
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses
+import java.net.URI
 import javax.ws.rs.*
 import javax.ws.rs.core.Application
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
 
 @OpenAPIDefinition(
     info = Info(
         title = "Greetings API",
-        description = "Manage Greetings to people identified by their unique name.",
+        description = "Manage Greetings to people identified by their unique name",
         version = "0.0.1",
         contact = Contact(name = "Gerald Loeffler", url = "http://integrational.eu")
     )
@@ -24,15 +32,31 @@ class API : Application()
 interface GreetingsAPI {
     @Operation(
         summary = "Add a Greeting",
-        description = "Add a Greeting from the representation in the request body, where the given name must be unique, returning the complete added Greeting."
+        description = "Add a Greeting from the representation in the request body, where the given name must be unique, returning the complete added Greeting"
+    )
+    @APIResponses(
+        APIResponse(
+            responseCode = "201",
+            description = "Greeting was added",
+            headers = [Header(
+                name = "Location",
+                description = "Absolute URI of the added Greeting",
+                schema = Schema(implementation = URI::class)
+            )]
+        ),
+        APIResponse(
+            responseCode = "409",
+            description = "Greeting was not added because of duplicate name",
+            content = [Content(schema = Schema(implementation = ErrorResponse::class))]
+        )
     )
     @POST
     @Consumes(APPLICATION_JSON)
-    fun add(toAdd: GreetingToAdd): Greeting
+    fun add(toAdd: GreetingToAdd, @Context uriInfo: UriInfo): Response
 
     @Operation(
         summary = "Get a Greeting by name",
-        description = "Get the Greeting with the given name, if any, where the name is case-insensitive."
+        description = "Get the Greeting with the given name, if any, where the name is case-insensitive"
     )
     @GET
     @Path("/{name}")
@@ -40,7 +64,7 @@ interface GreetingsAPI {
 
     @Operation(
         summary = "Get all Greetings",
-        description = "Get all Greetings, in no particular order."
+        description = "Get all Greetings, in no particular order"
     )
     @GET
     fun getAll(): Collection<Greeting>
@@ -64,4 +88,7 @@ data class Greeting(
     val message: String
 ) : GreetingCore()
 
-data class ErrorResponse(val message: String)
+data class ErrorResponse(
+    @get:Schema(description = "Explanation of the error", example = "Duplicate name 'Isaac Newton'")
+    val message: String
+)
--- src/main/kotlin/org/integrational/greetings/restapi/GreetingsResource.kt
+++ src/main/kotlin/org/integrational/greetings/restapi/GreetingsResource.kt
@@ -1,21 +1,31 @@
 package org.integrational.greetings.restapi
 
+import org.integrational.greetings.domain.repo.DuplicateNameException
 import org.integrational.greetings.domain.service.GreetingsService
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
+import javax.ws.rs.ext.ExceptionMapper
+import javax.ws.rs.ext.Provider
 import org.integrational.greetings.domain.model.Greeting as DomainGreeting
 
 @ApplicationScoped
 class GreetingsResource @Inject constructor(private val svc: GreetingsService) : GreetingsAPI {
-    override fun add(toAdd: GreetingToAdd) = fromDomain(svc.add(toAdd.name))
+    override fun add(toAdd: GreetingToAdd, uriInfo: UriInfo) =
+        svc.add(toAdd.name).let { Response.created(uriOfAdded(it, uriInfo)).build() }
+
     override fun getByName(name: String) = svc.get(name)?.let { fromDomain(it) }
     override fun getAll() = svc.getAll().map { fromDomain(it) }
 
     companion object {
+        private fun uriOfAdded(dg: DomainGreeting, ui: UriInfo) = ui.absolutePathBuilder.path(dg.name).build()
         private fun fromDomain(dg: DomainGreeting) = Greeting(dg.name, dg.message)
     }
+}
 
-//    @ExceptionHandler
-//    fun handleDuplicateName(ex: DuplicateNameException): ResponseEntity<ErrorResponse> =
-//        ResponseEntity.badRequest().body(ErrorResponse("Duplicate name '${ex.name}'"))
+@Provider
+class DuplicateNameExceptionMapper : ExceptionMapper<DuplicateNameException> {
+    override fun toResponse(ex: DuplicateNameException): Response =
+        Response.status(Response.Status.CONFLICT).entity(ErrorResponse("Duplicate name '${ex.name}'")).build()
 }

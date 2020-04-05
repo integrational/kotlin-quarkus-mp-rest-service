67f799e19b317b63e65d985f770c7f764c5c9452
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-04-05 09:44:00 +0200

    completed OpenAPI annotations

--- src/main/kotlin/org/integrational/greetings/restapi/GreetingsAPI.kt
+++ src/main/kotlin/org/integrational/greetings/restapi/GreetingsAPI.kt
@@ -4,6 +4,7 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.info.Contact
 import org.eclipse.microprofile.openapi.annotations.info.Info
+import org.eclipse.microprofile.openapi.annotations.media.Schema
 import javax.ws.rs.*
 import javax.ws.rs.core.Application
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
@@ -11,6 +12,7 @@ import javax.ws.rs.core.MediaType.APPLICATION_JSON
 @OpenAPIDefinition(
     info = Info(
         title = "Greetings API",
+        description = "Manage Greetings to people identified by their unique name.",
         version = "0.0.1",
         contact = Contact(name = "Gerald Loeffler", url = "http://integrational.eu")
     )
@@ -21,16 +23,16 @@ class API : Application()
 @Produces(APPLICATION_JSON)
 interface GreetingsAPI {
     @Operation(
-        summary = "Add Greeting",
-        description = "Add a Greeting from the representation in the request body, where the given name must be unique."
+        summary = "Add a Greeting",
+        description = "Add a Greeting from the representation in the request body, where the given name must be unique, returning the complete added Greeting."
     )
     @POST
     @Consumes(APPLICATION_JSON)
     fun add(toAdd: GreetingToAdd): Greeting
 
     @Operation(
-        summary = "Get Greeting by name",
-        description = "Get the Greeting with the given name, or none."
+        summary = "Get a Greeting by name",
+        description = "Get the Greeting with the given name, if any, where the name is case-insensitive."
     )
     @GET
     @Path("/{name}")
@@ -38,7 +40,7 @@ interface GreetingsAPI {
 
     @Operation(
         summary = "Get all Greetings",
-        description = "Get all Greetings."
+        description = "Get all Greetings, in no particular order."
     )
     @GET
     fun getAll(): Collection<Greeting>
@@ -48,7 +50,18 @@ sealed class GreetingCore {
     abstract val name: String
 }
 
-data class GreetingToAdd(override val name: String) : GreetingCore()
-data class Greeting(override val name: String, val message: String) : GreetingCore()
+@Schema(description = "Representation of a new Greeting to add")
+data class GreetingToAdd(
+    @get:Schema(description = "Name of the person to greet", example = "Isaac Newton")
+    override val name: String
+) : GreetingCore()
+
+@Schema(description = "Representation of a complete Greeting")
+data class Greeting(
+    @get:Schema(description = "Name of the person to greet", example = "Isaac Newton")
+    override val name: String,
+    @get:Schema(description = "Message to greet the person with", example = "Hello Isaac Newton!")
+    val message: String
+) : GreetingCore()
 
 data class ErrorResponse(val message: String)
